5ce8e37b5c82de95f50b77c62e4e5aed181d7a26
KNI-Harry <124707289+KNI-Harry@users.noreply.github.com>
2023-02-07 20:51:53 +0000

    Delete create_database.py

--- create_database.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('Users.db')
-print("Opened database successfully");
-
-conn.execute('CREATE TABLE people(Email TEXT, Username TEXT, Password TEXT)')
-print("Table created successfully");
-conn.close()
